3adaec685026224759d705e2cddc95a17d05b4ac
Arthur <arthurfellipees@gmail.com>
2024-04-08 09:47:36 -0300

    exemplo desestruturacao, arrow function

--- script.js
+++ script.js
@@ -24,5 +24,41 @@ const listaAlunos = [
 
 for(aluno of listaAlunos){
     let media=(aluno.nota[0] + aluno.nota[1] / 2);
-    document.write(`A media do aluno ${aluno.nome} é ${media}`)
+    console.log(`A media do aluno ${aluno.nome} é ${media}`)
 }
+
+/* SORTEAR NUMERO */
+
+const sorteio = [];
+
+while(sorteio.length <6){
+    let numero1 =Math.ceil(Math.random() * 60)
+    if(!sorteio.includes(numero1)){
+        sorteio.push(numero1)
+    }
+}
+sorteio.sort((a,b)=>a-b);
+console.log(sorteio);
+
+
+
+const usuario ={
+    nome:"Fiap",
+    Local:"São paulo",
+    idade:20
+};
+//sem desestruturacao
+console.log(`Nome e Local: ${usuario.nome} ${usuario.local}`)
+
+//com desestruturacao
+let {Nome,Local} = usuario;
+console.log(`${Nome} ${Local}`)
+
+//arrow function
+let items =["Fusca","Belina","Gurgel","Brasilia","Kombi"];
+items.forEach(function(item){
+    console.log(item)
+})
+
+let items1 = ["Fusca-1","Belina1","Gurgel1","Brasilia1","Kombi1"];
+items1.forEach(item1 =>console.log(item1))
