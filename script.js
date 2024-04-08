3b25af57b1aef4644a2087ac8d9bdabcc8a08556
Arthur <arthurfellipees@gmail.com>
2024-04-08 09:01:18 -0300

    criando javascript com array, objeto, templates strings

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ for(let numero of listaNumeros){
 }
 console.log("pares", pares);
 console.log("impares", impares);
+
+/*com uma lista de alunos vamos percorer cada aluno e trazer a media da nota e mostrar o nome do aluno e sua media*/
+
+const listaAlunos = [
+    {nome:"Huguinho", nota:[5,7]},
+    {nome:"Zezinho", nota:[6,9]},
+    {nome:"Luizinho", nota:[7,8]}
+]
+
+for(aluno of listaAlunos){
+    let media=(aluno.nota[0] + aluno.nota[1] / 2);
+    document.write(`A media do aluno ${aluno.nome} é ${media}`)
+}
